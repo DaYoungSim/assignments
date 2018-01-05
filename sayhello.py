4b859a0fcb8effdd9aca15bcdbab11ee75fe6571
DaYoung Sim <das9899@student.hks.harvard.edu>
2018-01-05 16:35:16 -0500

    add comments to sayhello.py

--- sayhello.py
+++ sayhello.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python
+# this program outputs a greeting to the terminal
+# taking the name
 import sys
 name = sys.stdin.read()
+
+# printing Hello <input name> on the screen
 print "Hello "+ name + "!"
