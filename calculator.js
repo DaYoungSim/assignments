dda22c166f05979b5002c6e5c4f0692a7c91dfb4
DaYoung Sim <das9899@student.hks.harvard.edu>
2018-01-08 20:09:53 -0500

    print the result of 5 times 6

--- calculator.js
+++ calculator.js
@@ -13,3 +13,7 @@ function multiply(a,b){
 function divide(a,b){
 	return (a / b)
 }
+
+console.log("I'm going to use the calculator function to multiply 5 and 6")
+var x = multiply(5,6)
+console.log(x)
