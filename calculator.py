4e53fd717aa46b07f2631fd4e922dd5e378d1c8c
DaYoung Sim <das9899@student.hks.harvard.edu>
2018-01-08 19:49:03 -0500

    add square and cube function to calculator

--- calculator.py
+++ calculator.py
@@ -10,6 +10,21 @@ def multiply(a,b):
 def divide(a,b):
 	return a / b
 
+def sqaure(a):
+	return a * 2
+
+def cube(a):
+	return a * 3
+
 print "I'm going to use the calculator functions to multiply 5 and 6"
 x = multiply(5,6)
 print x
+
+
+print "Square of 5 is:"
+y = square(5)
+print y
+
+print "Cube of 5 is:"
+z = cube(5)
+print z
