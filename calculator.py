51cd729da19e963ed5448da5bca953b37c6b2a00
DaYoung Sim <das9899@student.hks.harvard.edu>
2018-01-08 19:36:20 -0500

    print the result of 5 times 6

--- calculator.py
+++ calculator.py
@@ -4,9 +4,12 @@ def add(a,b):
 def subtract(a,b):
 	return a - b
 
-def multipliy(a,b):
+def multiply(a,b):
 	return a * b
 
 def divide(a,b):
 	return a / b
 
+print "I'm going to use the calculator functions to multiply 5 and 6"
+x = multiply(5,6)
+print x
